a0e5bccd2324b0037f17c9835532bd8fcce6a297
Zengor <ialmeida@edu.unifor.br>
2021-12-02 21:25:30 +0900

    Day 2

--- rust/src/days/day2.rs
+++ rust/src/days/day2.rs
@@ -1,8 +1,58 @@
-#![allow(dead_code)]
+use itertools::Itertools;
 
-pub fn part1(_input: &str) -> impl ToString {
-    return "unimplemented";
+struct Position {
+    x: u32,
+    y: u32,
+    aim: u32,
 }
-pub fn part2(_input: &str) -> impl ToString {
-    return "unimplemented";
+
+impl Position {
+    fn new() -> Self {
+        Self { x: 0, y: 0, aim: 0 }
+    }
+
+    fn with_move(mut self, (dir, val): (&str, u32)) -> Self {
+        match dir {
+            "forward" => self.x += val,
+            "down" => self.y += val,
+            "up" => self.y -= val,
+            _ => panic!("Invalid input"),
+        }
+        self
+    }
+    fn with_aim_move(mut self, (dir, val): (&str, u32)) -> Self {
+        match dir {
+            "forward" => {
+                self.x += val;
+                self.y += self.aim * val
+            }
+            "down" => self.aim += val,
+            "up" => self.aim -= val,
+            _ => panic!("Invalid input"),
+        }
+        self
+    }
+
+    fn pos_value(&self) -> u32 {
+        self.x * self.y
+    }
+}
+
+fn instructions(input: &str) -> impl Iterator<Item = (&str, u32)> {
+    input
+        .lines()
+        .map(|line| line.trim().split(' ').collect_tuple().unwrap())
+        .map(|(dir, val)| (dir, val.parse::<u32>().unwrap()))
+}
+
+pub fn part1(input: &str) -> impl ToString {
+    instructions(input)
+        .fold(Position::new(), Position::with_move)
+        .pos_value()
+}
+
+pub fn part2(input: &str) -> impl ToString {
+    instructions(input)
+        .fold(Position::new(), Position::with_aim_move)
+        .pos_value()
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,16 +2,9 @@ use aoc2021::*;
 
 fn main() {
     println!("Hello, world!");
-    println!(
-        "{}",
-        day1::part1(include_str!("../../input/01-1.txt")).to_string()
-    );
-    println!(
-        "{}",
-        day1::part2(include_str!("../../input/01-1.txt")).to_string()
-    );
-    println!(
-        "{}",
-        day1::part2_noitertools(include_str!("../../input/01-1.txt")).to_string()
-    );
+    let p1 = day2::part1;
+    let p2 = day2::part2;
+    let input = include_str!("../../input/02-1.txt");
+    println!("{}", p1(input).to_string());
+    println!("{}", p2(input).to_string());
 }
